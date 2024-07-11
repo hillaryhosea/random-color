1b51830039126e92c1ecfdcbcbd7fbc30f555d46
hillaryhosea <107868525+hillaryhosea@users.noreply.github.com>
2024-07-11 23:54:01 +0300

    inital commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Randomcolor } from './components/randomcolor';
+
+function App() {
+  return(
+    <div >
+      <Randomcolor />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/randomcolor.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './randomcolor.css'
+
+export const Randomcolor = () => {
+  return (
+    <div className='container'>
+        <button>Create HEX Color</button>
+        <button>Create RGB Color</button>
+        <button> Genarate Random Color</button>
+    </div>
+  )
+}
